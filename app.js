70a58f4e9dba52756b8a32e42d1fd4d488ab0390
unknown <mehdi.rvcs@gmail.com>
2022-04-17 09:10:51 +0500

    swagger docs added

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
-// Swagger
-// const swaggerUI = require('swagger-ui-express');
-// const YAML = require('yamljs');
-// const swaggerDocument = YAML.load('./swagger.yaml');
+Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 
 const express = require('express');
@@ -44,7 +44,7 @@ app.use(xss());
 app.get('/', (req, res) => {
   res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
 });
-// app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 
 // routes
