627ce152486f4e94a8dc2587b17a2e3cd5626e33
unknown <mehdi.rvcs@gmail.com>
2022-04-17 09:17:55 +0500

    swagger docs added

--- app.js
+++ app.js
@@ -42,9 +42,9 @@ app.use(cors());
 app.use(xss());
 
 app.get('/', (req, res) => {
-  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>')
 });
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 
 // routes
