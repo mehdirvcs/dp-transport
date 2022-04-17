349db1498d2911e547c1705baf500ebd7afdd1c8
unknown <mehdi.rvcs@gmail.com>
2022-04-17 08:20:50 +0500

    2nd commit

--- app.js
+++ app.js
@@ -41,9 +41,9 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
-// app.get('/', (req, res) => {
-//   res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
-// });
+app.get('/', (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
 // app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 
